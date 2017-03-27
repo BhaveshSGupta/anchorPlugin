51528aad8beda563c6149752b5240f0a9932582e
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2017-03-27 14:39:25 +0530

    First Update

--- anchorplugin.js
+++ anchorplugin.js
@@ -1,3 +1,13 @@
-$.fn.anchorplugin = function() {
-
-};
+(function( $ ) {
+ $.fn.anchorplugin = function() {
+ Array.from( document.querySelectorAll( 'a' ) ).forEach( a => {
+    a.classList.add( location.hostname === a.hostname || !a.hostname.length ? 'apLocal' : 'apExternal' );
+});
+        this.filter( "a.apExternal").each(function() {
+            //var link = $(this).attr("href");
+            //var current_addr = window.location.hostname;
+            $j(this).attr({target:"_blank",title:"Opens in a new window"});
+        });
+        return this;
+    };
+}($j));
