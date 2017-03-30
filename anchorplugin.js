931db7231a023907a85005551769a72b13500877
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2017-03-30 16:21:24 +0530

    spaces around braches

--- anchorplugin.js
+++ anchorplugin.js
@@ -1,9 +1,9 @@
-(function( $ ) {
+(function($) {
     $.fn.anchorplugin = function() {
-        Array.from( document.querySelectorAll( 'a' ) ).forEach( a => {
+        Array.from(document.querySelectorAll('a')).forEach( a => {
             a.classList.add( location.hostname === a.hostname || !a.hostname.length ? 'apLocal' : 'apExternal' );
         });
-        this.filter( "a.apExternal").each(function() {
+        this.filter("a.apExternal").each(function() {
             $(this).attr({target:"_blank",title:"Opens in a new window"});
         });
         return this;
