4a768214526ea2147d3ac7a70f4bc14749955a79
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2017-03-30 14:51:45 +0530

    Updated and Cleaned

--- anchorplugin.js
+++ anchorplugin.js
@@ -1,13 +1,11 @@
 (function( $ ) {
- $.fn.anchorplugin = function() {
- Array.from( document.querySelectorAll( 'a' ) ).forEach( a => {
-    a.classList.add( location.hostname === a.hostname || !a.hostname.length ? 'apLocal' : 'apExternal' );
-});
+    $.fn.anchorplugin = function() {
+        Array.from( document.querySelectorAll( 'a' ) ).forEach( a => {
+            a.classList.add( location.hostname === a.hostname || !a.hostname.length ? 'apLocal' : 'apExternal' );
+        });
         this.filter( "a.apExternal").each(function() {
-            //var link = $(this).attr("href");
-            //var current_addr = window.location.hostname;
-            $j(this).attr({target:"_blank",title:"Opens in a new window"});
+            $(this).attr({target:"_blank",title:"Opens in a new window"});
         });
         return this;
     };
-}($j));
+}(jQuery));
